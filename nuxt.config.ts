ff29bf0cf13198ffd245409b50c2dae43c03b219
Boudot Romain <boudotromain@gmail.com>
2022-10-16 22:12:46 +0200

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  css: [
+    '~/assets/css/tailwind.css',
+    '~/assets/css/main.css',
+  ],
+  build: {
+    postcss: {
+      postcssOptions: require('./postcss.config.ts'),
+    },
+  }
+})
